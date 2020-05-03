622f907b80a4ea9b498df1796d53bbcfeb1c58e6
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-03 11:26:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
 
+function nowServing(katzDeliLine){
+  if(katzDeliLine.shift = undefined){
+    return 'There is nobody waiting to be served!'
+  }
+  else{
+    return `Currently serving ${katzDeliLine.shift}!`
+  }
 }
