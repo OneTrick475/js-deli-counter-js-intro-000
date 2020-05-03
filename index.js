4adbc213bc5f6355c074f702489015f0618c6f60
Ivan Ivanov Bukev <ibukev4@gmail.com>
2020-05-03 11:53:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine){
     return "The line is currently empty."
   }
   else{
-    for(i=0; i<katzDeliLine.length; i++){
+    for(var i=0; i<katzDeliLine.length; i++){
       var newarray = ['The line is currently:'];
        newarray.push(` ${i += 1}. ${katzDeliLine[i]},`);
       }
